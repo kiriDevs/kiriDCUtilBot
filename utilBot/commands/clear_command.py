67d70a051248e0481bc3c3ab242980788ced1b9a
kiriDevs <56218513+kiriDevs@users.noreply.github.com>
2021-02-20 01:37:45 +0100

    feat: Adding basic function: "clear" command
    Also moved python files from python/ to utilBot/

---
+++ utilBot/commands/clear_command.py
@@ -0,0 +1,28 @@
+from typing import List
+
+from discord import Message, TextChannel
+from discord.ext.commands import Context
+
+
+async def clear_command(ctx: Context, message_amount_string: str):
+    if not isinstance(ctx.channel, TextChannel):
+        await ctx.send(
+            "This command can only be used in a TextChannel on a server!"
+        )
+        return
+
+    try:
+        message_amount: int = int(message_amount_string)
+    except ValueError:
+        await ctx.send("Please enter a valid number of messages to delete!")
+        return
+
+    message_amount += 1  # Adding one for the message that contains the command
+
+    deleted_messages: List[Message] = await ctx.channel.purge(
+        limit=message_amount
+    )
+    deleted_number: int = len(deleted_messages)
+    await ctx.send(
+        f"**Success**! Deleted {deleted_number} messages!", delete_after=3
+    )
---
+++ utilBot/main.py
@@ -0,0 +1,22 @@
+from discord.ext.commands import Bot, Context
+from yaml import safe_load as load_yaml
+
+from utilBot.commands.clear_command import clear_command
+
+with open("../config/credentials.yml", "r") as credential_file:
+    AUTH_TOKEN: str = load_yaml(credential_file)["DISCORD_BOT_TOKEN"]
+
+with open("../config/config.yml", "r") as config_file:
+    raw_prefix: str = load_yaml(config_file)["prefix"]
+    raw_prefix_with_space: str = raw_prefix + " "
+    PREFIXES: tuple = (raw_prefix_with_space, raw_prefix)
+
+bot: Bot = Bot(command_prefix=PREFIXES, case_insensitive=True)
+
+
+@bot.command(name="clear")
+async def clear_chat(ctx: Context, message_amount_string: str):
+    await clear_command(ctx, message_amount_string)
+
+
+bot.run(AUTH_TOKEN)
