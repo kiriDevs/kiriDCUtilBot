a9d114e7554f189b69dc9743c5b7ecf3f5ce982f
kiriDevs <56218513+kiriDevs@users.noreply.github.com>
2021-02-28 12:38:12 +0100

    feat: Implementing raw_reaction_add event for self-role-assignment

--- utilBot/main.py
+++ utilBot/main.py
@@ -1,7 +1,10 @@
+# -*- coding: UTF-8 -*-
+from discord import RawReactionActionEvent
 from discord.ext.commands import Bot, Context
 from yaml import safe_load as load_yaml
 
 from utilBot.commands.clear_command import clear_command
+from utilBot.events.raw_reaction_add import raw_reaction_add
 
 with open("../config/credentials.yml", "r") as credential_file:
     AUTH_TOKEN: str = load_yaml(credential_file)["DISCORD_BOT_TOKEN"]
@@ -22,5 +25,10 @@ bot: Bot = Bot(command_prefix=PREFIXES, case_insensitive=True)
 async def clear_chat(ctx: Context, message_amount_string: str):
     await clear_command(ctx, message_amount_string)
 
+
+@bot.event
+async def on_raw_reaction_add(payload: RawReactionActionEvent):
+    global REACTION_DATA_TREE
+    await raw_reaction_add(payload, REACTION_DATA_TREE, bot)
 
 bot.run(AUTH_TOKEN)
