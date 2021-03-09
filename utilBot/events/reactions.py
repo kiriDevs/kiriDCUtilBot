0a12af52105d61e411984565efbfa0b7f72b2eb6
kiriDevs <56218513+kiriDevs@users.noreply.github.com>
2021-03-09 10:06:24 +0100

    feat: Implement raw_reaction_remove for self-role-removal
    Also renamed utilBot/events/raw_reaction_add to utilBot/events/reactions to contain both events in one appropriately-named file.

--- utilBot/events/reactions.py
+++ utilBot/events/reactions.py
@@ -33,3 +33,29 @@ async def raw_reaction_add(
                 f"Invalid roleID '{grant_role_id}' for reaction "
                 + "'{emoji_name}' on message {message_id}!"
             )
+
+
+async def raw_reaction_remove(
+    payload: RawReactionActionEvent, data_tree: dict, client: Client
+):
+    assert isinstance(payload.emoji.name, str)
+    emoji_name: str = payload.emoji.name
+    message_id: int = payload.message_id
+
+    if data_tree[message_id][emoji_name]:
+        assert isinstance(payload.guild_id, int)
+        guild_id: int = payload.guild_id
+        guild: Optional[Guild] = client.get_guild(guild_id)
+        assert isinstance(guild, Guild)
+
+        remove_role_id: int = data_tree[message_id][emoji_name]
+        remove_role: Optional[Role] = guild.get_role(remove_role_id)
+
+        if isinstance(remove_role, Role):
+            assert isinstance(payload.member, Member)
+            member: Member = payload.member
+            await member.remove_roles(
+                remove_role,
+                reason="Self-Removal using Reaction "
+                + f"'{emoji_name}' on message '{message_id}'",
+            )
